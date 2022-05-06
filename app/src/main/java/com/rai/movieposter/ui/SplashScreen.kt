27c71a678ed3720390878be27e1639067129adb6
Denis Zabolotski <morsmes@gmail.com>
2022-05-06 21:42:06 +0300

    splashScreen

---
+++ app/src/main/java/com/rai/movieposter/ui/SplashScreen.kt
@@ -0,0 +1,38 @@
+package com.rai.movieposter.ui
+
+import android.R
+import android.app.Activity
+import android.content.Intent
+import android.os.Bundle
+import android.os.Handler
+import android.view.View
+import android.view.WindowManager
+import androidx.appcompat.app.AppCompatActivity
+import com.rai.movieposter.databinding.ActivityMainBinding
+import com.rai.movieposter.databinding.ActivitySplashBinding
+
+
+class SplashScreen : AppCompatActivity()  {
+
+    private lateinit var binding: ActivitySplashBinding
+
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+        binding = ActivitySplashBinding.inflate(layoutInflater)
+        val view = binding.root
+        setContentView(view)
+        window.setFlags(WindowManager.LayoutParams.FLAG_FULLSCREEN,
+            WindowManager.LayoutParams.FLAG_FULLSCREEN)
+
+        with(binding){
+            imgBackgroud.animate().translationY(-2500.0f).setDuration(1000).setStartDelay(5000)
+            appMade.animate().translationY(2000.0f).setDuration(1000).setStartDelay(5000)
+            animationView.animate().translationY(1500.0f).setDuration(1000).setStartDelay(5000)
+        }
+
+
+
+        Handler().postDelayed({ startActivity(Intent(this@SplashScreen, MainActivity::class.java)) },
+            6000)
+    }
+}
